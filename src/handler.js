c9604f62a5069b325d5d9c5729e490a6796e42e4
rvnltf <aldirivan56@gmail.com>
2021-05-06 15:04:19 +0700

    Menambahkan dan Menampilkan semua buku

---
+++ src/handler.js
@@ -0,0 +1,65 @@
+const {nanoid} = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+    const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
+
+    const id = nanoid(12);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+    const finished = pageCount===readPage||false;
+
+    const newBook = {
+        id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
+    };
+
+    books.push(newBook);
+
+    const isNotNull = books.filter((book)=>book.name==='').length>0;
+    const isGreater = books.filter((book)=>book.readPage>book.pageCount).length>0;
+    const isSuccess = books.filter((book)=>book.id===id).length>0;
+
+    if (isSuccess) {
+        if (isNotNull) {
+            const response = h.response({
+                status: 'fail',
+                message: 'Gagal menambahkan buku. Mohon isi nama buku'
+            });
+            response.code(400);
+            return response;
+        }
+        if (isGreater) {
+            const response = h.response({
+                status: 'fail',
+                message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+            });
+            response.code(400);
+            return response;
+        }
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil ditambahkan',
+            data: {
+                bookId: id
+            }
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal ditambahkan'
+    })
+    response.code(500);
+    return response;
+}
+
+const getAllBook = (request, h) => ({
+    status: 'success',
+    data: {
+        books
+    }
+})
+
+module.exports = {addBookHandler, getAllBook};
